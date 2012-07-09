cfdee767fdae1535e51ec0f542cefbeca0157ff8
ryan moore <rmoore4146@gmail.com>
2012-07-08 22:05:21 -0400

    Initial commit for a Maven3 simple example program

---
+++ Hackathong/src/main/java/dix/walton/moore/activity/Application.java
@@ -0,0 +1,18 @@
+package dix.walton.moore.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import dix.walton.moore.R;
+
+
+public class Application extends Activity {
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        }
+
+ 
+}
